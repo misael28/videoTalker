90da2d010b2d40c6da4e98fc26e746e00a131d02
misael <misael.geof@gmail.com>
2021-11-06 16:31:00 -0300

    refactor: change project structure

--- talker-front/src/App.js
+++ talker-front/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { connectWithWebSocket } from "./utils/wssConnection/wwsConnection";
-import LoginPage from "./pages/Login/login";
-import Dashboard from "./pages/Dashboard/dashboard";
+import LoginPage from "../src/LoginPage/LoginPage";
+import Dashboard from "../src/Dashboard/Dashboard";
 
 function App() {
   useEffect(() => {
