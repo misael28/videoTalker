9c2a97ad5aa94777a0e9264a9004466b2fa03c4c
misael <misael.geof@gmail.com>
2021-11-06 01:08:40 -0300

    feat: create connection with serve by useEffect

--- talker-front/src/App.js
+++ talker-front/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+import { useEffect } from "react";
+import { connectWithWebSocket } from "./utils/wssConnection/wwsConnection";
 
 function App() {
+  useEffect(() => {
+    connectWithWebSocket();
+  }, []);
   return <div className="App"></div>;
 }
 
