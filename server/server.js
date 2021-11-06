3b602306320d875c3910d1b2e8a5cf2d92a77656
misael <misael.geof@gmail.com>
2021-11-06 00:42:53 -0300

    chore: init server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import socket from "socket.io";
+
+const PORT = 5000;
+
+const app = express();
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", POST],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.emit("connection", null);
+  console.log("new user connected");
+  console.log(socket.id);
+});
