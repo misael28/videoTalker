93b28ea37ff125d9169e09633a271c8ea819538e
misael <misael.geof@gmail.com>
2021-11-06 01:28:32 -0300

    feat: create routers

--- talker-front/src/App.js
+++ talker-front/src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useEffect } from "react";
 import { connectWithWebSocket } from "./utils/wssConnection/wwsConnection";
+import LoginPage from "./pages/Login/login";
+import Dashboard from "./pages/Dashboard/dashboard";
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
   }, []);
-  return <div className="App"></div>;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/" component={LoginPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
